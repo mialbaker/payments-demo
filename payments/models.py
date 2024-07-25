bcbf3ca192e10d1f94a65bc263ee7255edaef2f0
Mia Baker <miabaker@Jonahs-MacBook-Pro.local>
2024-07-25 14:35:04 -0700

    remove extra func

--- payments/models.py
+++ payments/models.py
@@ -8,5 +8,3 @@ class Payments(models.Model):
     provider_id = models.CharField(max_length=10, default="", blank=True, null=True)
     employee_type_id = models.IntegerField(default=1, blank=True, null=True)
 
-    def __str__(self):
-        return self.paytype_id
